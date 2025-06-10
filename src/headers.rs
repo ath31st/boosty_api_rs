2a39d4c05fb313b94c2af24db034f9c4730e44f3
ath31st <ath31st@yandex.ru>
2025-06-10 20:31:55 +0300

    create headers.rs mod, add dependencies for lib

---
+++ src/headers.rs
@@ -0,0 +1,29 @@
+use anyhow::Result;
+use reqwest::header::{AUTHORIZATION, HeaderMap, HeaderValue};
+
+pub struct Headers {
+    map: HeaderMap,
+}
+
+impl Headers {
+    pub fn new_with_auth(access_token: &str) -> Result<Self> {
+        let mut map = HeaderMap::new();
+        let auth_value = HeaderValue::from_str(&format!("Bearer {}", access_token))?;
+        map.insert(AUTHORIZATION, auth_value);
+        Ok(Headers { map })
+    }
+
+    pub fn new() -> Self {
+        Headers {
+            map: HeaderMap::new(),
+        }
+    }
+
+    pub fn insert(
+        &mut self,
+        key: impl Into<reqwest::header::HeaderName>,
+        value: impl Into<HeaderValue>,
+    ) {
+        self.map.insert(key.into(), value.into());
+    }
+}
