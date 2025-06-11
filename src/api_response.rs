20b1db1703270d48411d2b57b65be5980b61d108
ath31st <ath31st@yandex.ru>
2025-06-11 11:21:02 +0300

    change modifier for api response struct

--- src/api_response.rs
+++ src/api_response.rs
@@ -3,158 +3,160 @@ use serde::Deserialize;
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ApiResponse {
-    user: User,
-    is_pinned: bool,
-    is_blocked: bool,
-    has_access: bool,
-    data: Vec<MediaData>,
-    is_record: bool,
-    content_counters: Vec<ContentCounter>,
-    donators: Donators,
-    show_views_counter: bool,
-    created_at: i64,
-    is_published: bool,
-    is_liked: bool,
-    tags: Vec<String>,
-    is_comments_denied: bool,
-    count: Count,
-    publish_time: i64,
-    title: String,
-    sort_order: i64,
-    price: i32,
-    id: String,
-    comments: Comments,
-    donations: i32,
-    teaser: Vec<String>,
-    is_waiting_video: bool,
-    int_id: i64,
-    is_deleted: bool,
-    updated_at: i64,
-    signed_query: String,
-    advertiser_info: Option<serde_json::Value>,
-    currency_prices: CurrencyPrices,
+    pub user: User,
+    pub is_pinned: bool,
+    pub is_blocked: bool,
+    pub has_access: bool,
+    pub data: Vec<MediaData>,
+    pub is_record: bool,
+    pub content_counters: Vec<ContentCounter>,
+    pub donators: Donators,
+    pub show_views_counter: bool,
+    pub created_at: i64,
+    pub is_published: bool,
+    pub is_liked: bool,
+    pub tags: Vec<String>,
+    pub is_comments_denied: bool,
+    pub count: Count,
+    pub publish_time: i64,
+    pub title: String,
+    pub sort_order: i64,
+    pub price: i32,
+    pub id: String,
+    pub comments: Comments,
+    pub donations: i32,
+    pub teaser: Vec<String>,
+    pub is_waiting_video: bool,
+    pub int_id: i64,
+    pub is_deleted: bool,
+    pub updated_at: i64,
+    pub signed_query: String,
+    pub advertiser_info: Option<serde_json::Value>,
+    pub currency_prices: CurrencyPrices,
 }
 
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct User {
-    blog_url: String,
-    avatar_url: String,
-    name: String,
-    has_avatar: bool,
-    id: i64,
-    flags: Flags,
+pub struct User {
+    pub blog_url: String,
+    pub avatar_url: String,
+    pub name: String,
+    pub has_avatar: bool,
+    pub id: i64,
+    pub flags: Flags,
 }
 
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct Flags {
-    show_post_donations: bool,
+pub struct Flags {
+    pub show_post_donations: bool,
 }
 
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct VideoData {
-    upload_status: String,
+pub struct VideoData {
+    pub upload_status: String,
+    pub width: u32,
+    pub status: String,
+    pub title: String,
+    pub url: String,
+    pub preview_id: String,
+    pub player_urls: Vec<PlayerUrl>,
+    pub id: String,
+    pub vid: String,
+    pub preview: String,
+    pub height: u32,
+    pub time_code: i32,
+    pub show_views_counter: bool,
+    pub duration: u32,
+    pub complete: bool,
+    pub views_counter: u32,
+    pub default_preview: String,
+    pub failover_host: String,
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ImageData {
+    pub url: String,
+    pub width: u32,
+    pub height: u32,
+    pub preview: Option<String>,
+    pub id: String,
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct PlayerUrl {
     #[serde(rename = "type")]
-    type_: String,
-    width: u32,
-    status: String,
-    title: String,
-    url: String,
-    preview_id: String,
-    player_urls: Vec<PlayerUrl>,
-    id: String,
-    vid: String,
-    preview: String,
-    height: u32,
-    time_code: i32,
-    show_views_counter: bool,
-    duration: u32,
-    complete: bool,
-    views_counter: u32,
-    default_preview: String,
-    failover_host: String,
+    pub type_: String,
+    pub url: String,
 }
 
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct ImageData {
-    url: String,
-    width: u32,
-    height: u32,
-    preview: Option<String>,
-    id: String,
-}
-
-#[derive(Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct PlayerUrl {
+pub struct ContentCounter {
     #[serde(rename = "type")]
-    type_: String,
-    url: String,
+    pub type_: String,
+    pub count: u32,
+    pub size: u64,
 }
 
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct ContentCounter {
-    #[serde(rename = "type")]
-    type_: String,
-    count: u32,
-    size: u64,
+pub struct Donators {
+    pub extra: ExtraFlag,
+    pub data: Vec<serde_json::Value>,
 }
 
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct Donators {
-    extra: ExtraFlag,
-    data: Vec<serde_json::Value>,
+pub struct ExtraFlag {
+    pub is_last: bool,
 }
 
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct ExtraFlag {
-    is_last: bool,
+pub struct Comments {
+    pub extra: ExtraFlag,
+    pub data: Vec<serde_json::Value>,
 }
 
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct Comments {
-    extra: ExtraFlag,
-    data: Vec<serde_json::Value>,
+pub struct Count {
+    pub comments: u32,
+    pub reactions: Reactions,
+    pub likes: u32,
 }
 
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct Count {
-    comments: u32,
-    reactions: Reactions,
-    likes: u32,
+pub struct Reactions {
+    pub dislike: u32,
+    pub heart: u32,
+    pub fire: u32,
+    pub angry: u32,
+    pub wonder: u32,
+    pub laught: u32,
+    pub sad: u32,
+    pub like: u32,
 }
 
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct Reactions {
-    dislike: u32,
-    heart: u32,
-    fire: u32,
-    angry: u32,
-    wonder: u32,
-    laught: u32,
-    sad: u32,
-    like: u32,
-}
-
-#[derive(Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct CurrencyPrices {
-    rub: f32,
-    usd: f32,
+pub struct CurrencyPrices {
+    pub rub: f32,
+    pub usd: f32,
 }
 
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase", tag = "type")]
-enum MediaData {
+pub enum MediaData {
+    #[serde(rename = "video", rename_all = "camelCase")]
     Video(VideoData),
+    #[serde(rename = "ok_video", rename_all = "camelCase")]
+    OkVideo(VideoData),
+    #[serde(rename = "image", rename_all = "camelCase")]
     Image(ImageData),
 }
