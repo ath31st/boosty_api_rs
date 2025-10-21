d13329f533a2d4cb202dd78e5609b76530b6c437
ath31st <ath31st@yandex.ru>
2025-10-21 14:16:15 +0300

    change processing comments, add new fn - get all comments with loop fetching while isLast and isFirst no true

--- src/api_client/comment.rs
+++ src/api_client/comment.rs
@@ -2,22 +2,26 @@ use reqwest::StatusCode;
 
 use crate::{
     api_client::ApiClient,
-    api_response::CommentsResponse,
+    api_response::{Comment, CommentsResponse},
     error::{ApiError, ResultApi},
 };
 
 impl ApiClient {
-    pub async fn get_comments(
+    pub async fn get_comments_response(
         &self,
         blog_name: &str,
         post_id: &str,
         limit: Option<u32>,
         reply_limit: Option<u32>,
         order: Option<&str>,
+        offset: Option<u64>,
     ) -> ResultApi<CommentsResponse> {
         let mut path = format!("blog/{blog_name}/post/{post_id}/comment/");
 
         let mut params = Vec::new();
+        if let Some(o) = offset {
+            params.push(format!("offset={o}"));
+        }
         if let Some(l) = limit {
             params.push(format!("limit={l}"));
         }
@@ -53,5 +57,43 @@ impl ApiClient {
         })?;
 
         Ok(parsed)
+    }
+
+    pub async fn get_all_comments(
+        &self,
+        blog_name: &str,
+        post_id: &str,
+        limit: Option<u32>,
+        reply_limit: Option<u32>,
+        order: Option<&str>,
+    ) -> ResultApi<Vec<Comment>> {
+        let mut all_comments = Vec::new();
+        let mut offset: Option<u64> = None;
+
+        loop {
+            let resp = self
+                .get_comments_response(blog_name, post_id, limit, reply_limit, order, offset)
+                .await?;
+
+            if resp.data.is_empty() {
+                break;
+            }
+
+            let last_id = resp.data.last().map(|c| c.int_id);
+
+            all_comments.extend(resp.data);
+
+            if resp.extra.is_last && resp.extra.is_first {
+                break;
+            }
+
+            if let Some(id) = last_id {
+                offset = Some(id);
+            } else {
+                break;
+            }
+        }
+
+        Ok(all_comments)
     }
 }
